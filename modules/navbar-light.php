7037181b9063a5c0d8282f980973312c3c6f5988
Favour Okoh <okohfavour91@gmail.com>
2023-07-18 22:31:07 +0100

    Fixed href links from navbar

--- modules/navbar-light.php
+++ modules/navbar-light.php
@@ -1,18 +1,18 @@
 <header>
     <nav class="navbar navbar-expand-md m-0 p-0 w-100">
         <div class="container navbar-container mt-0">
-            <a class="navbar-brand navbar-logo-wrapper" href="index.php" tabindex="-1">
+            <a class="navbar-brand navbar-logo-wrapper" href="index.html" tabindex="-1">
                 <img src="assets/images/survey-bay-logo.png" alt="Survey Bay Logo" width="155px">
             </a>
 
             <div class="navbar-text-container d-none d-md-flex">
-                <a href="index.php#respondents-section" class="navbar-text-wrapper">
+                <a href="index.html#respondents-section" class="navbar-text-wrapper">
                     <div class="navbar-text">For Respondents</div>
                 </a>
-                <a href="index.php#businesses-section" class="navbar-text-wrapper">
+                <a href="index.html#businesses-section" class="navbar-text-wrapper">
                     <div class="navbar-text">For Businesses</div>
                 </a>
-                <a href="about.php" class="navbar-text-wrapper">
+                <a href="about.html" class="navbar-text-wrapper">
                     <div class="navbar-text">About Us</div>
                 </a>
             </div>
@@ -21,7 +21,7 @@
                 <select name="language">
                     <option value="english">EN</option>
                     <option value="french">FR</option>
-                    <option value="portugese">PO</option>
+                    <option value="portuguese">PO</option>
                 </select>
             </div>
 
